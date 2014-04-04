0ee6f031f16bed5cd80f26c8c6ccd3f23fadefb9
Takeshi Ueda <uedatakeshi@gmail.com>
2014-04-05 06:36:30 +0900

    ruby rackup sinatra bundle sample

---
+++ sinatra/README.md
@@ -0,0 +1,6 @@
+ruby_practice
+=============
+gem install bundler
+bundle install
+bundle exec rackup
+
--- sinatra/app.rb
+++ sinatra/app.rb
@@ -1,7 +1,21 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
+	configure :development do
+		puts 'development mode'
+	end
+
+	configure :production do
+		puts 'production mode'
+	end
+
+	configure do
+		puts 'all mode'
+	end
+
 	get '/' do
+		binding.pry
 		'hi!!'
 	end
 end
